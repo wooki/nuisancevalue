663dc148e18c730ba899ea0411dbc40e6727781c
Jim Rowe <jim@jimcode.org>
2020-08-20 12:42:51 +0100

    faction relations for some complex scenatios

--- src/common/Factions.js
+++ src/common/Factions.js
@@ -1,33 +1,112 @@
 // hard-coded reference data
 export default class Factions {
 
-	// set up all the factions - these can't be changed in game
+	// set up all the factions
   constructor() {
 
 		this.friendly = 0;
 		this.neutral = 1;
 		this.hostile = 2;
 
-		this.independentIndex = 0;
-		this.spaceForceIndex = 1;
-		this.piratesIndex = 2;
+		this.independent = 0; // often the player - usualy neutral/friendly to all
+		this.spaceForce = 1; // galactic police
+		this.pirates = 2; // enemy to all
+    this.colonists = 3; // often preyed on by corps or pirates
+    this.russian = 4; // national, at war with ferrous
+    this.russianWar = 5; // national, at war with commonwealth & ferrous
+    this.chninese = 6; // national
+    this.chnineseWar = 7; // national, at war with corporations
+    this.commonwealth = 8; // national, at war with colonists
+    this.mikkei = 9; // corporation, Mikkei Combine at war with ferrous and russian
+    this.jupiter = 10; // corporation, JMC Jupiter Mining Corp
+    this.ferrous = 11; // corporation, FC Ferrous Corp, at war with colonists and mikkei
 
+    this.defaultRelations = [1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 		this.factions = [];
 
-		this.factions[this.independentIndex] = {
+		this.factions[this.independent] = {
 			name: 'Independent',
-			relations: [1, 1, 2]
+			relations: this.defaultRelations.slice()
 		}
+    this.factions[this.independent].relations[this.colonists] = this.friendly;
 
-		this.factions[this.spaceForceIndex] = {
+
+		this.factions[this.spaceForce] = {
 			name: 'Space Force',
-			relations: [1, 0, 2]
+      relations: this.defaultRelations.slice()
+		}
+    this.factions[this.spaceForce].relations[this.russianWar] = this.hostile;
+    this.factions[this.spaceForce].relations[this.chnineseWar] = this.hostile;
+
+
+    this.factions[this.pirates] = {
+			name: 'Pirates',
+      relations: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
 		}
 
-		this.factions[this.piratesIndex] = {
-			name: 'Pirates',
-			relations: [1, 2, 2]
+    this.factions[this.colonists] = {
+			name: 'Colonists',
+      relations: this.defaultRelations.slice()
 		}
+    this.factions[this.colonists].relations[this.independent] = this.friendly;
+    this.factions[this.colonists].relations[this.commonwealth] = this.hostile;
+    this.factions[this.colonists].relations[this.ferrous] = this.hostile;
+
+    this.factions[this.russian] = {
+			name: 'Russian Federation',
+      relations: this.defaultRelations.slice()
+		}
+    this.factions[this.russian].relations[this.ferrous] = this.hostile;
+
+    this.factions[this.russianWar] = {
+			name: 'Russian Federation',
+      relations: this.defaultRelations.slice()
+		}
+    this.factions[this.russianWar].relations[this.ferrous] = this.hostile;
+    this.factions[this.russianWar].relations[this.commonwealth] = this.hostile;
+
+    this.factions[this.chninese] = {
+			name: 'People\'s Republic of China',
+      relations: this.defaultRelations.slice()
+		}
+
+    this.factions[this.chnineseWar] = {
+			name: 'People\'s Republic of China',
+      relations: this.defaultRelations.slice()
+		}
+    this.factions[this.chnineseWar].relations[this.mikkei] = this.hostile;
+    this.factions[this.chnineseWar].relations[this.jupiter] = this.hostile;
+    this.factions[this.chnineseWar].relations[this.ferrous] = this.hostile;
+
+    this.factions[this.commonwealth] = {
+			name: 'Commonwealth of Nations',
+      relations: this.defaultRelations.slice()
+		}
+    this.factions[this.commonwealth].relations[this.colonists] = this.hostile;
+    this.factions[this.commonwealth].relations[this.russianWar] = this.hostile;
+
+    this.factions[this.mikkei] = {
+			name: 'Mikkei Combine',
+      relations: this.defaultRelations.slice()
+		}
+    this.factions[this.mikkei].relations[this.chnineseWar] = this.hostile;
+    this.factions[this.mikkei].relations[this.ferrous] = this.hostile;
+
+    this.factions[this.jupiter] = {
+			name: 'Jupiter Mining Corporation',
+      relations: this.defaultRelations.slice()
+		}
+    this.factions[this.mikkei].relations[this.chnineseWar] = this.hostile;
+
+    this.factions[this.ferrous] = {
+			name: 'Ferrous Corp',
+      relations: this.defaultRelations.slice()
+		}
+    this.factions[this.ferrous].relations[this.russian] = this.hostile;
+    this.factions[this.ferrous].relations[this.russianWar] = this.hostile;
+    this.factions[this.ferrous].relations[this.chnineseWar] = this.hostile;
+    this.factions[this.ferrous].relations[this.mikkei] = this.hostile;
+
 	}
 
 	getFaction(id) {
