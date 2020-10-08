acb457248b675e9416879bc1e64cc12475818a58
Jim Rowe <jim@jimcode.org>
2020-10-08 11:49:38 +0100

    debugging NaN

--- src/client/NvRenderer.js
+++ src/client/NvRenderer.js
@@ -127,4 +127,41 @@ export default class NvRenderer extends Renderer {
 
     }
 
+    runClientStep(t) {
+        let p = this.clientEngine.options.stepPeriod;
+        let dt = 0;
+
+        // reset step time if we passed a threshold
+        if (this.doReset || t > this.clientEngine.lastStepTime + TIME_RESET_THRESHOLD) {
+            this.doReset = false;
+            this.clientEngine.lastStepTime = t - p / 2;
+            this.clientEngine.correction = p / 2;
+        }
+
+        // catch-up missed steps
+        while (t > this.clientEngine.lastStepTime + p) {
+            this.clientEngine.step(this.clientEngine.lastStepTime + p, p + this.clientEngine.correction);
+            this.clientEngine.lastStepTime += p;
+            this.clientEngine.correction = 0;
+        }
+
+        // if not ready for a real step yet, return
+        // this might happen after catch up above
+        if (t < this.clientEngine.lastStepTime) {
+            dt = t - this.clientEngine.lastStepTime + this.clientEngine.correction;
+            console.log("NvRenderer dt:"+dt+" (set to zero)");
+            if (dt < 0) dt = 0;
+            this.clientEngine.correction = this.clientEngine.lastStepTime - t;
+            this.clientEngine.step(t, dt, true);
+            return;
+        }
+
+        // render-controlled step
+        dt = t - this.clientEngine.lastStepTime + this.clientEngine.correction;
+        console.log("NvRenderer dt:"+dt);
+        this.clientEngine.lastStepTime += p;
+        this.clientEngine.correction = this.clientEngine.lastStepTime - t;
+        this.clientEngine.step(t, dt);
+    }
+
 }
